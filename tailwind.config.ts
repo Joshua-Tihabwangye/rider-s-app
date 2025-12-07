1a90853a7d8aed11c9749d5316fd33eebd4b821e
snow virus <snowvirus.143.nt@gmail.com>
2025-12-07 17:33:58 +0300

    change from jsx to tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       colors: {
@@ -16,3 +17,6 @@ module.exports = {
   },
   plugins: []
 };
+
+export default config;
+
